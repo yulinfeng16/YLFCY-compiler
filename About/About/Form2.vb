283c826a12b5939370131dc853779613ed595b95
yulinfeng16 <foooyou541oo@163.com>
2022-06-16 10:36:05 +0800

    deletefiles

--- About/About/Form2.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Form2
-
-    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
-    End Sub
-End Class
--- CE.c
+++
@@ -1,8 +0,0 @@
-#include<windows.h>
-#include<stdio.h>
-int main() {
-	int hwndDOS = GetForegroundWindow();
-
-	ShowWindow(hwndDOS, SW_HIDE);
-	MessageBox(NULL,"转换出错。请稍后重试。","Oops!",MB_OK|MB_ICONWARNING);
-}
